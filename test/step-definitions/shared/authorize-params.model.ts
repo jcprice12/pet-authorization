95864bc9c46fdb8bebf10efb0f90e351718eea5c
jcprice12 <john.c.price12@gmail.com>
2021-09-05 20:29:54 -0400

    cleanup

--- test/step-definitions/shared/authorize-params.model.ts
+++
@@ -1,9 +0,0 @@
-import { Prompt } from '../../../src/authorize/prompt.enum';
-
-export interface AuthorizeParams {
-  response_type: ResponseType;
-  client_id: string;
-  redirect_uri: string;
-  scope?: string;
-  prompt?: Prompt;
-}
